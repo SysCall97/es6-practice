57faee6fcbc19efc79535837b690b70eb6d4d62d
Kazi Mashry <kazimashry@gmail.com>
2020-08-16 16:22:44 +0600

    allInOne fixed

--- allInOne.js
+++ allInOne.js
@@ -19,27 +19,34 @@ console.log("start");
 
 // backdated style of fetching
 
-// function fetchUserDetails (id, callBack) {
-//     setTimeout(() => {
-//         database.forEach(user => {
-//             if(user.id === id) callBack(user);
-//         });
-//     }, 1500);
-// }
+function fetchUserDetails (id, callBackAccept, callBackReject) {
+    setTimeout(() => {
+        let flag = false;
+        database.forEach(user => {
+            if(user.id === id && user.name !== "Shohag") { 
+                flag = true;
+                callBackAccept(user);
+            }
+        });
+        if(!flag) callBackReject(new Error("User not found!"));
+    }, 1500);
+}
 
-// function greetingUser (user, callBack) {
-//     setTimeout(() => {
-//         callBack(`Hello ${user.name}, Good morning.`);
-//     }, 1500);
-// }
+function greetingUser_1 (user, callBackAccept, callBackReject) {
+    setTimeout(() => {
+        if(user.name !== "Rakib") callBackAccept(`Hello ${user.name}, Good morning.`);
+        else callBackReject(new Error("Don't let him in"));
+    }, 1500);
+}
 
-// const user =  fetchUserDetails("CE-17037", (user) => {
-//     console.log(user.name);
-//     const greeting = greetingUser(user, (msg) => {
-//         console.log(msg);
-//     })
+function startProcess_1 (id) {
+    fetchUserDetails(id, user => {
+        greetingUser_1(user, (msg) => {
+            console.log("1-", msg);
+        }, error => console.log("1-", error.message))
 
-// });
+    }, error => console.log("1-", error.message));
+}
 
 
 // promise
@@ -69,27 +76,41 @@ function greetingUser (user) {
     })
 }
 
+function startProcess_2 (id) {
+    const user = fetchUser(id);
 
-const user = fetchUser("CE-17037");
-
-user
-.then(data => greetingUser(data))
-.then(msg => console.log(msg))
-.catch(error => console.log(error.message));
+    user
+    .then(data => greetingUser(data))
+    .then(msg => console.log("2-", msg))
+    .catch(error => console.log("2-", error.message));
+}
 
 
 // Sync style
-async function startProcess (id) {
+async function startProcess_3 (id) {
     try {
         const user = await fetchUser(id);
         const greeting = await greetingUser(user);
-        console.log(greeting);
+        console.log("3-", greeting);
     } catch (error) {
-        console.log(error.message);
+        console.log("3-", error.message);
     }
 }
 
-startProcess("CE-17038");
+
+
+// Test Drive
+startProcess_1("CE-17036");
+startProcess_1("CE-17037");
+startProcess_1("CE-17038");
+
+startProcess_2("CE-17036");
+startProcess_2("CE-17037");
+startProcess_2("CE-17038");
+
+startProcess_3("CE-17036");
+startProcess_3("CE-17037");
+startProcess_3("CE-17038");
 
 
 console.log("end");
